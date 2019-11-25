62489a2b122c033ad90aacd36b47452acdc7fa18
Steve Bell <53307050+Steve0Bell@users.noreply.github.com>
2019-11-25 21:57:36 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber1 * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1;
+
+document.querySelector(".img1").getAttribute("randomNumber1", "");
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+var randomNumber2 = Math.random();
+randomNumber2 = randomNumber2 * 6;
+randomNumber2 = Math.floor(randomNumber2) + 1;
+
+document.querySelector(".img2").getAttribute("randomNumber2", "");
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+
+if (randomNumber1 === randomNumber2) {
+  document.querySelector("#p1").innerHTML = "Players Draw";
+  document.querySelector("#p2").innerHTML = "Players Draw";
+}
+
+else if (randomNumber1 > randomNumber2) {
+  document.querySelector("#p1").innerHTML = "Players 1 Wins";
+}
+
+else if (randomNumber1 < randomNumber2) {
+  document.querySelector("#p2").innerHTML = "Player 2 Wins";
+}
